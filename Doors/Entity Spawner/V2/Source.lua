c800e4b5de5e4b4d6fd2e8e0c6f67c3f13a377b1
no12356 <157051087+no12356@users.noreply.github.com>
2026-09-23 04:16:27 +0300

    Update Source.lua

--- Doors/Entity Spawner/V2/Source.lua
+++ Doors/Entity Spawner/V2/Source.lua
@@ -1083,7 +1083,7 @@ gui.DisplayOrder = 999999
 gui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 bg.Name = "Background"
-bg.BackgroundColor3 = Color3.new(0, 0, 0)
+bg.BackgroundTransparency = 1
 bg.BorderSizePixel = 0
 bg.Size = UDim2.new(1, 0, 1, 0)
 bg.ZIndex = 999
